b07f6e73dd0f8160d46486be17c6647f0f7c5558
Marcus Dantas <marvidac.carvalho@unimedfortaleza.com.br>
2020-08-03 07:52:07 -0300

    Iniciando tela de escolha dos itens para selecionar seviços.

--- src/app/inspecao/inspecao-item-form/inspecao-item-form.page.ts
+++ src/app/inspecao/inspecao-item-form/inspecao-item-form.page.ts
@@ -1,15 +1,48 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Item } from '../../item/shared/item';
+import { ItemService } from '../../item/shared/item.service';
 
 @Component({
-  selector: 'app-inspecao-item-form',
-  templateUrl: './inspecao-item-form.page.html',
-  styleUrls: ['./inspecao-item-form.page.scss'],
+  selector: 'app-equipe-form',
+  templateUrl: './inspecao-equipe-form.page.html',
+  styleUrls: ['./inspecao-equipe-form.page.scss'],
 })
 export class InspecaoItemFormPage implements OnInit {
 
-  constructor() { }
+  idLocal:number;
+  idEquipe:number;
+
+  title: string = 'Nova Inspeção';
+
+  data: string;
+  
+  itens: Item[] = [];
+  
+  constructor(
+    private itemService: ItemService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit() {
+    console.log('ngOnInit inspecao-item-form.page.ts');
+    
+    this.idLocal = parseInt(this.route.snapshot.paramMap.get('idLocal'));
+    this.idEquipe = parseInt(this.route.snapshot.paramMap.get('idEquipe'));
+    
+    this.loadAllItens();
   }
 
+  loadAllItens(idItem?: number) {
+    this.geraListaDeItens();
+  }
+
+  limparFormulario() {
+    this.geraListaDeItens();
+  }
+  
+  async geraListaDeItens() {
+    this.itens = await this.itemService.getAll();
+  }
+  
 }
